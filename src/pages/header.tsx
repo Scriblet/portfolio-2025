e38d148dc4bb02eed79b3d2ce16b00e9bca3fabb
Lucas Nonato <46581168+Scriblet@users.noreply.github.com>
2025-03-29 01:01:52 -0300

    Update header styling and button layout

--- src/pages/header.tsx
+++ src/pages/header.tsx
@@ -15,10 +15,20 @@ export default function Header() {
   const onlyLargeScreen = useMediaQuery(theme.breakpoints.down("xl"));
 
   return (
-    <AppBar color="transparent" elevation={0}>
+    <AppBar
+      position="fixed"
+      elevation={0}
+      sx={{
+        backgroundColor: "rgba(11, 6, 19, 0.4)", // Fundo semi-transparente
+        boxShadow: "none",
+        backdropFilter: "blur(10px)",
+        WebkitBackdropFilter: "blur(10px)",
+        zIndex: 1000,
+      }}
+    >
       <Toolbar variant="dense">
         <Typography
-          variant="h6"
+          variant="h4"
           component={Link}
           to="/"
           sx={{
@@ -31,10 +41,28 @@ export default function Header() {
         >
           Lucas N.
         </Typography>
-        <Button color="inherit" sx={{ mx: 0.5, px: 2.5, minHeight: "6vh" }}>
+        <Button
+          color="inherit"
+          sx={{
+            mx: 0.5,
+            px: 2.5,
+            minHeight: "6vh",
+            fontSize: "1.1vw",
+            letterSpacing: 0.15,
+          }}
+        >
           Experiência
         </Button>
-        <Button color="inherit" sx={{ mx: 0.5, px: 2.5, minHeight: "6vh" }}>
+        <Button
+          color="inherit"
+          sx={{
+            mx: 0.5,
+            px: 2.5,
+            minHeight: "6vh",
+            fontSize: "1.1vw",
+            letterSpacing: 0.15,
+          }}
+        >
           Habilidades
         </Button>
         {/* <Button
@@ -51,6 +79,8 @@ export default function Header() {
             marginLeft: "auto",
             boxShadow: "none",
             fontWeight: "bold",
+            fontSize: "1.1vw",
+            letterSpacing: 0.15,
             minHeight: onlySmallScreen
               ? "20vh"
               : onlyMediumScreen
@@ -60,12 +90,12 @@ export default function Header() {
               : "8vh",
             borderRadius: 0,
             minWidth: onlySmallScreen
-            ? "10vw"
-            : onlyMediumScreen
-            ? "10vw"
-            : onlyLargeScreen
-            ? "10vw"
-            : "8vw",
+              ? "10vw"
+              : onlyMediumScreen
+              ? "10vw"
+              : onlyLargeScreen
+              ? "10vw"
+              : "8vw",
           }}
         >
           Contato
