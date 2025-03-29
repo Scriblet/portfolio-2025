e5f2d77d92d44a3f465e12dd461e47fda2363e64
Lucas Nonato <46581168+Scriblet@users.noreply.github.com>
2025-03-29 11:27:15 -0300

    Update Vite config to include additional SCSS modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   css: {
     modules: {
-      localsConvention: "camelCase",
+      localsConvention: 'camelCase'
     },
     preprocessorOptions: {
-      scss: {},
-    },
-  },
-});
+      scss: {
+        additionalData: `
+          @use "sass:list";
+          @use "sass:map";
+          @use "sass:math";
+          @use "sass:meta";
+          @use "sass:string";
+          // Outros módulos Sass que você usa
+        `
+      }
+    }
+  }
+})
