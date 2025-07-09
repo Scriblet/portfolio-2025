ef72a62e2da25b90589fe719b80722bd5e42b0e3
Lucas Nonato <46581168+Scriblet@users.noreply.github.com>
2025-07-09 19:40:25 -0300

    fix: Remover a importação da fonte @fontsource/poppins do arquivo main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "./theme";
-import "@fontsource/poppins";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import "./styles/responsive-utils.css";
